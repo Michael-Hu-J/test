13035ad1ac4379083dd388ba1493e97ab770b9b6
hujian <1103986830>
2020-07-09 23:04:09 +0800

    update

---
+++ test/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    Test test = new Test();
+    public static void main(String[] args) {
+//        int i = 128;
+//        byte b = (byte)i;
+        System.out.println((byte)Integer.MAX_VALUE);
+    }
+}
